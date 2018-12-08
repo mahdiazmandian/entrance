10cb81550d71f9348c5b32e1fa1acb78937d0d6c
Mahdi PI <mahdiazmandian@gmail.com>
2018-12-07 18:53:45 -0800

    added two reference images for of elevator and preliminary opencv test files.

---
+++ geanytest.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+def draw_circle(event,x,y,flags,param):
+    if event == cv.EVENT_LBUTTONDBLCLK:
+        cv.circle(img,(x,y),100,(255,0,0),-1)
+        print 'clicked: ({}, {})'.format(x, y)
+        
+#~ # Create a black image
+#~ img = np.zeros((512,512,3), np.uint8)
+#~ # Draw a diagonal blue line with thickness of 5 px
+#~ cv.line(img,(0,0),(511,511),(255,0,0),5)
+
+#~ cv.rectangle(img,(384,0),(510,128),(0,255,0),3)
+
+#~ cv.circle(img,(447,63), 63, (0,0,255), -1)
+
+#~ cv.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
+
+#~ pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
+#~ pts = pts.reshape((-1,1,2))
+#~ cv.polylines(img,[pts],True,(0,255,255))
+
+#~ font = cv.FONT_HERSHEY_SIMPLEX
+#~ #cv.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv.LINE_AA)
+
+#~ #cv.imwrite('drawing.png',img)
+#~ cv.imshow('image',img)
+#~ cv.waitKey(0)
+#~ cv.destroyAllWindows()
+
+#~ img = cv.imread('door_open.jpg',0)
+img = cv.imread('door_closed.jpg')
+cv.circle(img, (303, 52), 5, (0, 255, 0), 1)
+
+
+
+
+cv.namedWindow('image')
+cv.setMouseCallback('image',draw_circle)
+cv.imshow('image',img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
