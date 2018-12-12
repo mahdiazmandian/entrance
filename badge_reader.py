ea5b189d472b120193da5a22229fcfea5834370a
Mahdi <mahdi.azmandian2@am.sony.com>
2018-12-12 13:48:42 -0800

    Added speech when badge is scanned

--- badge_reader.py
+++ badge_reader.py
@@ -1,4 +1,22 @@
-import subprocess, time
+import subprocess, time, win32com.client as wincl
+
+
+mahdi_id ='6044779E'
+katrine_id = 'C87FA30C'
+nick_id = '6044779E'
+
+def sayNameOfID (id):
+    name = 'Guest'
+    if id == mahdi_id:
+        name = 'Matty'
+    elif id == katrine_id:
+        name = 'Katrin'
+    elif id == nick_id:
+        name = 'Nick'
+    speak = wincl.Dispatch("SAPI.SpVoice")
+    speak.Speak("Welcome {}".format(name))
+
+
 
 while True:
     proc = subprocess.Popen('cmdpcprox -getactiveid  -waitforgetactiveid=3600', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1)
@@ -10,5 +28,6 @@ while True:
         s = s.replace(" ", "")
         s = s[:8]
         print "Badge Read: {}".format(s)
+        sayNameOfID(s)
         # PROCESS BADGE DATA HERE
-        time.sleep(1)
+        # time.sleep(0.8)
